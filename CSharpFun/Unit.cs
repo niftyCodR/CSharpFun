18e2f0493eebe7a349cb4a8db6d7e9ef860142dc
niftyCodR <niftycodr@outlook.com>
2019-02-20 16:52:35 +0100

    Add Unit type

---
+++ CSharpFun/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpFun
+{
+    /// <summary>
+    /// Represents the absence of a value. This is similar to the void return type
+    /// in methods, but can be used in multiple places.
+    /// Enables an easier transformation model so you can model an <see cref="Action"/>
+    /// as a <see cref="Func{Unit}"/> of type <see cref="Unit"/>.
+    /// </summary>
+    public struct Unit
+    {
+        public static Unit Value { get; } = default;
+    }
+}
