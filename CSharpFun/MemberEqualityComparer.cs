10d22d0183ed3866e93577ef149899a3b4f9d3a8
niftyCodR <niftycodr@outlook.com>
2019-02-20 17:06:21 +0100

    Add ValueObject type

---
+++ CSharpFun/MemberEqualityComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpFun
+{
+    public sealed class MemberEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, IEnumerable<object>> _getEqualityMembers;
+
+        public MemberEqualityComparer(Func<T, IEnumerable<object>> getEqualityMembers)
+        {
+            _getEqualityMembers = getEqualityMembers ?? throw new ArgumentNullException(nameof(getEqualityMembers));
+        }
+
+        public bool Equals(T a, T b)
+        {
+            if(a == null || b == null || a.GetType() != b.GetType())
+                return false;
+
+            var members = _getEqualityMembers(a);
+            var otherMembers = _getEqualityMembers(b);
+
+            return members.SequenceEqual(otherMembers);
+        }
+
+        public int GetHashCode(T a)
+        {
+            unchecked
+            {
+                return _getEqualityMembers(a).Aggregate(0, (acc, val) => acc * 397 + val?.GetHashCode() ?? 0);
+            }
+        }
+    }
+}
---
+++ CSharpFun/ValueObject.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CSharpFun
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        private static readonly IEqualityComparer<ValueObject> EqualityComparer = new MemberEqualityComparer<ValueObject>(o => o.GetEqualityMembers());
+
+        protected virtual IEnumerable<object> GetEqualityMembers()
+        {
+            return PublicMemberUtil.GetPublicMembersFunc(GetType()).Invoke(this);
+        }
+
+        public bool Equals(ValueObject other)
+        {
+            return EqualityComparer.Equals(this, other);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ValueObject other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer.GetHashCode(this);
+        }
+
+        public static bool operator ==(ValueObject a, ValueObject b)
+        {
+            return EqualityComparer.Equals(a, b);
+        }
+
+        public static bool operator !=(ValueObject a, ValueObject b)
+        {
+            return !(a == b);
+        }
+
+        private static class PublicMemberUtil
+        {
+            private static readonly ConcurrentDictionary<Type, Func<object, IEnumerable<object>>> Cache = new ConcurrentDictionary<Type, Func<object, IEnumerable<object>>>();
+
+            public static Func<object, IEnumerable<object>> GetPublicMembersFunc(Type type)
+            {
+                return Cache.GetOrAdd(type, GetEqualityMembersFunc);
+            }
+
+            private static Func<object, IEnumerable<object>> GetEqualityMembersFunc(Type type)
+            {
+                var properties = type.GetProperties()
+                    .Where(p => p.CanRead && p.DeclaringType == typeof(ValueObject))
+                    .Cast<MemberInfo>();
+
+                var fields = type.GetFields()
+                    .Where(f => f.IsInitOnly && !f.IsStatic)
+                    .Cast<MemberInfo>();
+
+                var memberAccessors = properties.Union(fields)
+                    .Select(GetMemberAccessor)
+                    .ToList();
+
+                return valueObject => memberAccessors
+                    .Select(getter => getter(valueObject))
+                    .ToList();
+
+                Func<object, object> GetMemberAccessor(MemberInfo member)
+                {
+                    var param = Expression.Parameter(typeof(object), "source");
+                    // ReSharper disable once AssignNullToNotNullAttribute
+                    var source = Expression.Convert(param, member.DeclaringType);
+                    var get = Expression.MakeMemberAccess(source, member);
+                    var convert = Expression.Convert(get, typeof(object));
+                    var lambda = Expression.Lambda<Func<object, object>>(convert, param);
+
+                    return lambda.Compile();
+                }
+            }
+        }
+    }
+
+    public abstract class SingleValueObject<T> : ValueObject
+    {
+        protected SingleValueObject(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+
+        protected override IEnumerable<object> GetEqualityMembers()
+        {
+            yield return Value;
+        }
+
+        public static implicit operator T(SingleValueObject<T> valueObject) => valueObject.Value;
+    }
+}
