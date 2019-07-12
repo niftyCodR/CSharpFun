2b52bf044a5a6051407640a035d23e87a4f22223
niftyCodR <niftycodr@outlook.com>
2019-07-12 14:17:19 +0200

    Add equality comparison to Unit
    To avoid boxing in certain szenarios.

--- CSharpFun/Unit.cs
+++ CSharpFun/Unit.cs
@@ -8,8 +8,27 @@ namespace CSharpFun
     /// Enables an easier transformation model so you can model an <see cref="Action"/>
     /// as a <see cref="Func{Unit}"/> of type <see cref="Unit"/>.
     /// </summary>
-    public struct Unit
+    public struct Unit : IEquatable<Unit>
     {
         public static Unit Value { get; } = default;
+
+        public bool Equals(Unit other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public static bool operator ==(Unit a, Unit b) => true;
+
+        public static bool operator !=(Unit a, Unit b) => false;
     }
 }
