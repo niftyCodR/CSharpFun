d300ef22e8491b8a4dfdfdd92fbaa498351283c2
niftyCodR <niftycodr@outlook.com>
2019-10-10 13:12:24 +0200

    Add new conversion from Result to Lst
    This enables to use a result to participate in a (enumerable
    based) LINQ statement.

--- CSharpFun/ResultLstExtensions.cs
+++ CSharpFun/ResultLstExtensions.cs
@@ -3,5 +3,13 @@
     public static class ResultLstExtensions
     {
         public static Result<T, Lst<TError>> ErrorsAsList<T, TError>(this Result<T, TError> result) => result;
+
+        public static Lst<T> AsLst<T, TError>(this Result<T, TError> result)
+        {
+            return result.Match(
+                value => new Lst<T>(value),
+                _ => new Lst<T>()
+            );
+        }
     }
 }
