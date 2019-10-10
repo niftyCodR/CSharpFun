417046a6d1f2994d6b65b0be2ad6b461b355685e
niftyCodR <niftycodr@outlook.com>
2019-10-10 08:14:18 +0200

    Add new conversion from Option to Lst
    This enables to use an option to participate in a (enumerable
    based) LINQ statement.

---
+++ CSharpFun/OptionLstExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpFun
+{
+    public static class OptionLstExtensions
+    {
+        public static Lst<T> ToLst<T>(this Option<T> option)
+        {
+            return option.Match(
+                value => new Lst<T>(value),
+                () => new Lst<T>()
+            );
+        }
+    }
+}
